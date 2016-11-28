de523244920916c6c39706b6afacfc95a187cfca
anujshrestha47 <anujshrestha47@gmail.com>
2016-11-28 10:25:12 +0545

    Add files via upload

---
+++ sub.c
@@ -0,0 +1,8 @@
+/*
+   Student Name: Anuj Shrestha
+   Subject: Programing Fundamentals
+   Roll:
+   Program: Write a C program to enter marks of seven subjects and calculate.
+   Lab:07
+   Date:28/11/2016
+*/
