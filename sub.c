482807a74a497fcfa6350802a3c8c92c933805e2
anujshrestha47 <anujshrestha47@gmail.com>
2016-11-28 10:48:22 +0545

    Delete sub.c

--- sub.c
+++
@@ -1,8 +0,0 @@
-/*
-   Student Name: Anuj Shrestha
-   Subject: Programing Fundamentals
-   Roll:
-   Program: Write a C program to enter marks of seven subjects and calculate.
-   Lab:07
-   Date:28/11/2016
-*/
