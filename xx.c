8f9f0b5607de775daf7a4a28fef4eba52f325773
anujshrestha47 <anujshrestha47@gmail.com>
2016-11-28 12:46:00 +0545

    Add files via upload

---
+++ xx.c
@@ -0,0 +1,23 @@
+int main ()
+{
+    int a,b,x;
+    printf("enter length \n");
+    scanf("%d",&a);
+    printf("enter breadth \n");
+    scanf("%d",&b);
+
+
+
+    x=a*b;
+    printf("area of rectangle is :%d ",x);
+
+
+
+
+
+
+getch();
+return 0;
+
+
+}
