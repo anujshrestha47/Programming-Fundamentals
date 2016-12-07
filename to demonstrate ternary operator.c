b226df7ed1c8210b7abe131ddbf091213ded3205
anujshrestha47 <anujshrestha47@gmail.com>
2016-12-07 11:44:45 +0545

    Add files via upload

---
+++ to demonstrate ternary operator.c
@@ -0,0 +1,22 @@
+/*
+name:anuj shrestha
+roll no:05
+subject:programming fundamentals
+program:To check either the given no is odd or even using ternary operator.
+date:7th dec
+*/
+
+#include<stdio.h>
+ int main()
+ 
+ {
+	 int a;
+	 printf("enter any number \n");
+	 scanf("%d",&a);
+	 (a%2==0)? printf("%d is even",a): printf("%d is odd",a);
+	  return 0;
+	 
+	 
+	 
+ }
+	 
