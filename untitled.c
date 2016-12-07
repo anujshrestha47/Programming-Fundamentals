73dcdce61dd61ad9e3b5e4242c2a540991e28a1d
anujshrestha47 <anujshrestha47@gmail.com>
2016-12-07 11:19:11 +0545

    Add files via upload

---
+++ untitled.c
@@ -0,0 +1,23 @@
+/*
+Student Name: kamal
+Roll:11
+Lab no. :04
+Program: To print evev or odd useing if esle
+Date: 18 octber, 2016
+*/
+#include<stdio.h>
+int main()
+{
+int a;
+printf("Enter a number:\n");
+scanf("%d",&a);
+if (a%2==0)
+{
+printf("given no. is even");
+}
+else
+{
+printf("given no. is odd");
+}
+return 0;
+}
