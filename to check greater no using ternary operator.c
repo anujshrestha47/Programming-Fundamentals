b0bb09a9add801cdc36516622ede3b9e4f3fcbed
anujshrestha47 <anujshrestha47@gmail.com>
2016-12-07 11:53:01 +0545

    Add files via upload

---
+++ to check greater no using ternary operator.c
@@ -0,0 +1,24 @@
+/*
+name:anuj shrestha
+roll no:05
+subject:programming fundamentals
+program:To check greater no using ternary operator.
+date:7th dec
+*/
+
+#include<stdio.h>
+ int main()
+ 
+ {
+	 int a,b;
+	 printf("enter 1st number \n");
+	 scanf("%d",&a);
+	 printf("enter 2nd number \n");
+	 scanf("%d",&b);
+	 (a-b>=0)? printf("%d is greater",a): printf("%d is greater",b);
+	  return 0;
+	 
+	 
+	 
+ }
+	 
