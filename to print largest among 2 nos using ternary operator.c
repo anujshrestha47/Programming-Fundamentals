cc27eaa16989096599ee462a944632bcc34ec373
anujshrestha47 <anujshrestha47@gmail.com>
2016-12-09 10:51:18 +0545

    Add files via upload

---
+++ to print largest among 2 nos using ternary operator.c
@@ -0,0 +1,25 @@
+/*
+name:anuj shrestha
+roll no:05
+subject: wap to check the largest among 2 no using ternary operator.
+lab sheet:11
+date:9th dec
+*/
+
+
+#include<stdio.h>
+#include<conio.h>
+int main ()
+{
+int a,b;
+printf("enter 1 st no.");
+scanf("%d",&a);
+printf("enter 2 nd no.");
+scanf("%d",&b);
+(a>=b)?printf("the largest no is %d",a):printf("the largest %d",b)
+getch();
+return 0;
+
+
+
+}
