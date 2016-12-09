285dd4994e5b2a2f13fc82ca749a8619afbdcd4d
anujshrestha47 <anujshrestha47@gmail.com>
2016-12-09 10:58:42 +0545

    Add files via upload

---
+++ to check the number is positive or negative.c
@@ -0,0 +1,30 @@
+/*
+Student name: anuj shrestha
+Subject: Programming Fundamental
+Roll:
+Program: WAP to check the number is positive or negative.
+Lab sheet no:
+04
+Date: 2016 Nov 23th
+*/
+
+#include<stdio.h>
+
+int main () {
+    int a;
+    printf("Enter the number:\n");
+    scanf("%d",&a);
+    if(a>0)
+    {
+        printf("The number is positive.");
+    }
+
+    else{
+
+            printf("The number is negative.");
+
+    }
+
+    return(0);
+
+}
