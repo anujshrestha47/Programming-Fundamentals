de5b65336bbb91999586e036b230270d4f7282b2
anujshrestha47 <anujshrestha47@gmail.com>
2016-12-09 10:35:58 +0545

    Add files via upload

---
+++ to print sum of three digits.c
@@ -0,0 +1,24 @@
+/*
+Student name: anuj shrestha
+Subject: Programming Fundamental
+Roll:
+Program: WAP to program to print sum of three digits number
+Lab sheet no:
+Date: 2016 Nov 23th
+*/
+#include<stdio.h>
+#include<conio.h>
+
+int main() {
+int a,b,c,sum;
+printf("Enter the first digit:\n");
+scanf("%d",&a);
+printf("Enter the second digit:\n");
+scanf("%d",&b);
+printf("Enter the third digit:\n");
+scanf("%d",&c);
+sum=a+b+c;
+printf("The sum is %d",sum);
+return(0);
+
+}
