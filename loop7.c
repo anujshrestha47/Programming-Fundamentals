4a37d36819302e4d731743d244c4756bfefdac9e
anujshrestha47 <anujshrestha47@gmail.com>
2017-01-16 11:37:59 +0545

    Add files via upload

---
+++ loop7.c
@@ -0,0 +1,25 @@
+/*
+student name:anuj shrestha
+roll no:06
+program:wap to display the loop pattern.
+       1234
+       5678	                               
+labsheet:18
+date:16th jan
+*/
+
+#include<stdio.h>            
+int main()                 
+{                          
+int i,j,a;
+a=1;
+for(i=1;i<=2;i++)
+{
+	for(j=1;j<=4;j++)
+	{
+	printf("%d",a);
+	a++;
+	}
+printf("\n");
+}
+}
