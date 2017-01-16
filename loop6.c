0aaeb9769322956293a1800a4470c6194bde7e16
anujshrestha47 <anujshrestha47@gmail.com>
2017-01-16 11:35:22 +0545

    Add files via upload

---
+++ loop6.c
@@ -0,0 +1,23 @@
+/*
+student name:anuj shrestha
+roll no:06
+program:wap to display the loop pattern.
+       123
+       123 	                               
+labsheet:18
+date:16th jan
+*/
+
+#include<stdio.h>            
+int main()                 
+{                          
+int i,j;
+for(i=1;i<=2;i++)
+{
+	for(j=1;j<=3;j++)
+	{
+	printf("%d",j);
+	}
+printf("\n");
+}
+}
